4b53f74dfb0fee0925b26c23bdf0038a03863ff3
Vinil Vadakkepurakkal <82436391+vinil-v@users.noreply.github.com>
2025-08-26 15:30:45 +0530

    Update gather_azhpc_vm_diagnostics.sh

--- Linux/src/gather_azhpc_vm_diagnostics.sh
+++ Linux/src/gather_azhpc_vm_diagnostics.sh
@@ -194,7 +194,7 @@ is_nvidia_sku() {
     [[ "$clean" =~ ^standard_nd40r?s_v2$ ]] ||
     [[ "$clean" =~ ^standard_nd96am?sr(_a100)?_v4$ ]] ||
     [[ "$clean" =~ ^standard_nd96isr_h100_v5$ ]] ||
-    [[ "$clean" =~ ^standard_nc80adi?s(_h100)?_v5$ ]] ||
+    [[ "$clean" =~ ^standard_nc(40|80)adi?s(_h100)?_v5$ ]] ||
     [[ "$clean" =~ ^standard_nc(24|48|96)ads(_a100)?_v4$ ]] ||
     \
     [[ "$clean" =~ ^standard_nv(6|12|24)$ ]] ||
